cad805e95d74d71118c7bee5ebcba12b379b7413
Xwyzworms <pratamaazmi1@gmail.com>
2023-07-04 22:53:22 +0700

    add important library and interoperability compose

--- MovieRecommendation/app/src/main/java/com/example/movierecommendation/MainActivity.kt
+++ MovieRecommendation/app/src/main/java/com/example/movierecommendation/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.movierecommendation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.movierecommendation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.MainActivityCompose.setContent {
+            MaterialTheme {
+                Surface() {
+                    Text("ASHU GAN")
+                }
+            }
+        }
+
     }
 }
